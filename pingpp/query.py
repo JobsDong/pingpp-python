59a4529dcfe2e77e3caadb2250ba51dba8884c63
Zhang Xuanyi <zhangxuanyi@tigerknows.com>
2014-11-28 14:44:26 +0800

    Change QuerySet to a descriptor.
    QuerySet should not be accessible from instance.

--- pingpp/query.py
+++ pingpp/query.py
@@ -9,6 +9,14 @@ __author__ = ['"wuyadong" <wuyadong311521@gmail.com>']
 
 class QuerySet(object):
 
+    def __get__(self, instance, type_=None):
+        """ QuerySet shouldn't be accessible from instance.
+        """
+        if instance is not None:
+            raise AttributeError("%s isn't accessible via %s instances" %
+                                 (self.__class__.__name__, type_.__name__))
+        return self
+
     def create(self):
         """ I think this method should be defined in Charge and Refund.
         QuerySet should only do query list. It returns a list of Charge objects
