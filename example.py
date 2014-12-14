ed9f259b587de49731566ef9ff53524799597202
Zhang Xuanyi <zhangxuanyi@tigerknows.com>
2014-12-14 20:01:19 +0800

    Update examples.

--- example.py
+++ example.py
@@ -1,50 +1,42 @@
-#!/usr/bin/python
-#-*- coding=utf-8 -*-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-""" ping plus plus 例子
-"""
-
-__author__ = ['"wuyadong" <wuyadong311521@gmail.com>']
-
+__author__ = ['"wuyadong" <wuyadong311521@gmail.com>', 'iceout']
 
 import pingpp
+import pingpp.http
+import requests
 
-pingpp.api_key = "sk_test_uHmDyTKWffX1enbXDSmLCKe5"
+pingpp.api_key = "KEY"
 
-ch = pingpp.Charge.objects.create(order_no="123ffffgdaf34", amount=10,
-                                  app={'id': 'app_1Kenv5f5GiDCKWLW'},
-                                  channel='upmp',
-                                  currency='cny',
-                                  client_ip='127.0.0.1',
-                                  subject='iphone',
-                                  body='hello')
-print ch.id, ch
-
-
-ch = pingpp.Charge.objects.get(id='ch_iH0yT4zLeDqT0i5qbPXzDaz1')
-print ch.id, ch
-
+ch = pingpp.Charge.objects.retrieve(charge_id='ch_id')
+print ch
 
 chs = pingpp.Charge.objects.all()
 print len(chs)
-for ch in chs:
-    print ch.id, ch
+# for ch in chs:
+#     print ch
 
-# TODO 模拟付款，之后再退款
-#
-
-re = pingpp.Refund.objects.create(charge_id=ch.id,
+ch = pingpp.Charge.objects.create(order_no="adf9fhr1hr3hhhj12a",
                                   amount=100,
-                                  description="hello")
-print re.id, re
+                                  app={'id': 'app_id'},
+                                  channel='upmp',
+                                  currency='cny',
+                                  client_ip='127.0.0.1',
+                                  subject='apple',
+                                  body='一天一个苹果')
+requests.get(
+    'https://api.pingplusplus.com/notify/charges/'+ch['id']+'?livemode=false')
+
+refund = pingpp.Refund.objects.create(charge_id=ch['id'],
+                                      amount=10,
+                                      description="太贵了,打个折")
+
+refund = pingpp.Refund.objects.retrieve(
+    charge_id=ch['id'],
+    refund_id=refund['id'])
+print refund, type(refund)
 
 
-re = pingpp.Refund.objects.get(charge_id=ch.id, refund_id=re.id)
-print re
-
-
-res = pingpp.Refund.objects.all(charge_id=ch.id)
-print len(res)
-
-for re in res:
-    print re.id, re
+refunds = pingpp.Refund.objects.all(charge_id=ch['id'])
+print len(refunds)
